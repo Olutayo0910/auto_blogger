a966580a4f1d7b42923d414129f3eefdf9e4f611
Olutayo0910 <olutayoogunlade0910@gmail.com>
2024-05-23 09:44:37 +0100

    login done

--- auto_blog_app/blog_generator/views.py
+++ auto_blog_app/blog_generator/views.py
@@ -1,13 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def user_login(request):
+    error_message = ''
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error_message': error_message})
     return render(request, 'login.html')
 
 def user_signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirmPassword = request.POST['confirmPassword']
+
+        if password == confirmPassword:
+            try:
+                # Create a new user
+                user = User.objects.create_user(username, email, password)
+                user.save()
+                login(request, user)
+                return redirect('/')
+            except:
+                error_message = 'Unable to create account'
+                return render(request, 'sign_up.html', {'error_message': error_message, 'email' : email, 'username' : username})
+
+        else:
+            error_message = 'password does not match'
+            return render(request, 'sign_up.html', {'error_message': error_message})
     return render(request, 'sign_up.html')
 
 def user_logout(request):
